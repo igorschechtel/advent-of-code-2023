0061158e8f1e45ec3ee6b27646925da94cc8dcd7
Igor Schechtel <igorpschechtel@gmail.com>
2024-02-27 00:50:48 -0300

    wip day 16

---
+++ src/16-the-floor-will-be-lava/part-1.ts
@@ -0,0 +1,83 @@
+import path from 'path';
+
+const filePath = path.join(import.meta.dir, 'example-input.txt');
+const file = Bun.file(filePath);
+const content = await file.text();
+
+const matrix = content.split('\n').map((row) => row.split(''));
+const energizedMatrix = matrix.map((row) => row.map(() => '.'));
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+type Instruction = { i: number; j: number; direction: Direction };
+const queue: Instruction[] = [];
+
+const beam = (i: number, j: number, direction: Direction) => {
+  console.log({ length: queue.length, i, j, direction });
+  // out of bounds
+  if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) return;
+
+  energizedMatrix[i][j] = '#';
+  const cell = matrix[i][j];
+
+  // empty space, continues in the same direction
+  if (cell === '.') {
+    if (direction === 'up') return queue.push({ i: i - 1, j: j, direction: 'up' });
+    else if (direction === 'down') return queue.push({ i: i + 1, j, direction: 'down' });
+    else if (direction === 'left') return queue.push({ i: i, j: j - 1, direction: 'left' });
+    else if (direction === 'right') return queue.push({ i: i, j: j + 1, direction: 'right' });
+  }
+
+  // mirror, reflected 90 degrees
+  else if (['/', '\\'].includes(cell)) {
+    if (cell === '/') {
+      if (direction === 'up') return queue.push({ i: i, j: j + 1, direction: 'right' });
+      else if (direction === 'down') return queue.push({ i: i, j: j - 1, direction: 'left' });
+      else if (direction === 'left') return queue.push({ i: i + 1, j, direction: 'down' });
+      else if (direction === 'right') return queue.push({ i: i - 1, j, direction: 'up' });
+    } else if (cell === '\\') {
+      if (direction === 'up') return queue.push({ i: i, j: j - 1, direction: 'left' });
+      else if (direction === 'down') return queue.push({ i: i, j: j + 1, direction: 'right' });
+      else if (direction === 'left') return queue.push({ i: i - 1, j, direction: 'up' });
+      else if (direction === 'right') return queue.push({ i: i + 1, j, direction: 'down' });
+    }
+  }
+
+  // splitter, passes through as empty space or creates 2 beams
+  else if (['|', '-'].includes(cell)) {
+    // vertical splitter
+    if (cell === '|') {
+      // split the beam in two
+      if (direction === 'right' || direction === 'left') {
+        queue.push({ i: i - 1, j, direction: 'up' });
+        return queue.push({ i: i + 1, j, direction: 'down' });
+      }
+      // continue in the same direction
+      else {
+        if (direction === 'up') return queue.push({ i: i, j: j - 1, direction: 'up' });
+        else if (direction === 'down') return queue.push({ i: i, j: j + 1, direction: 'down' });
+      }
+    }
+
+    // horizontal splitter
+    else {
+      // split the beam in two
+      if (direction === 'up' || direction === 'down') {
+        queue.push({ i: i, j: j - 1, direction: 'left' });
+        return queue.push({ i: i, j: j + 1, direction: 'right' });
+      }
+      // continue in the same direction
+      else {
+        if (direction === 'left') return queue.push({ i: i, j: j - 1, direction: 'left' });
+        else if (direction === 'right') return queue.push({ i: i, j: j + 1, direction: 'right' });
+      }
+    }
+  }
+};
+
+queue.push({ i: 0, j: 0, direction: 'right' });
+while (queue.length) {
+  const instruction = queue.shift();
+  if (!instruction) continue;
+  beam(instruction.i, instruction.j, instruction.direction);
+}
+console.log(energizedMatrix.map((row) => row.join('')).join('\n'));
